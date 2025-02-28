bfb88d1d7226ab84db0169eec772114046910460
Akaidus <nikolajjensen6854@gmail.com>
2025-02-28 14:54:01 +0100

    interaction is visualized with a button above the closest interactable target and an image is filled when the button is being held

--- Assets/Nikos trash/PlayerController.cs
+++ Assets/Nikos trash/PlayerController.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
     Vector3 movement;
-    bool canPlayerMove = true;
+    bool canPlayerAct = true;
     [SerializeField] float speed = 5f;
     CharacterController characterController;
     PlayerInput playerInput;
     List<GameObject> npcs = new List<GameObject>();
+    [SerializeField] GameObject interactButtonPrefab;
+    GameObject currentInteractButton;
+    Image interactButtonFillImage;
     [HideInInspector] public GameObject closestNPC = null;
+    [SerializeField] float interactFillTime = 0.5f;
 
     void Awake()
     {
@@ -37,8 +43,8 @@ public class PlayerController : MonoBehaviour
         playerInput.actions["Move"].performed += OnMove;
         playerInput.actions["Move"].canceled += OnMove;
         playerInput.actions["Interact"].performed += OnInteract;
-        PlayerInputEvent.FreezePlayer += () => canPlayerMove = false;
-        PlayerInputEvent.UnFreezePlayer += () => canPlayerMove = true;
+        PlayerInputEvent.FreezePlayer += () => canPlayerAct = false;
+        PlayerInputEvent.UnFreezePlayer += () => canPlayerAct = true;
     }
 
     void OnDisable()
@@ -46,8 +52,8 @@ public class PlayerController : MonoBehaviour
         playerInput.actions["Move"].performed -= OnMove;
         playerInput.actions["Move"].canceled -= OnMove;
         playerInput.actions["Interact"].performed -= OnInteract;
-        PlayerInputEvent.FreezePlayer -= () => canPlayerMove = false;
-        PlayerInputEvent.UnFreezePlayer -= () => canPlayerMove = true;
+        PlayerInputEvent.FreezePlayer -= () => canPlayerAct = false;
+        PlayerInputEvent.UnFreezePlayer -= () => canPlayerAct = true;
     }
     void OnMove(InputAction.CallbackContext context)
     {
@@ -57,21 +63,57 @@ public class PlayerController : MonoBehaviour
 
     void OnInteract(InputAction.CallbackContext context)
     {
-        IdentifyClosestNPC();
         if (closestNPC == null) return;
+        if (!canPlayerAct) return;
+        StartCoroutine(Interact(context));
+      
         Debug.Log($"Player interacted with {closestNPC.name}. Hello!");
-        PlayerInputEvent.OnPlayerInteract();
     }
 
     void Move()
     {
-        if (!canPlayerMove) return;
+        if (!canPlayerAct) return;
         if (characterController)
             characterController.Move(movement * speed * Time.deltaTime);
+    }
+
+    IEnumerator Interact(InputAction.CallbackContext context)
+    {
+        if(interactButtonFillImage == null) yield break;
+        var start = interactButtonFillImage.fillAmount;
+        var end = 1f;
+        float elapsedTime = 0;
+        // while the player holds the interact button, the fill amount of the interact button increases.
+        while (context.ReadValueAsButton() && elapsedTime < end)
+        {
+            if (interactButtonFillImage == null) yield break;
+            yield return null;
+            elapsedTime += Time.deltaTime / interactFillTime;
+            interactButtonFillImage.fillAmount = Mathf.Lerp(start, end, elapsedTime);
+        }
+        // if the player releases the interact button before the fill amount reaches 1, the interaction is cancelled.
+        if (!context.ReadValueAsButton())
+        {
+            interactButtonFillImage.fillAmount = 0;
+            yield break;
+        }
+        // if the player holds the interact button until the fill amount reaches 1, the interaction is successful.
+        start = end;
+        yield return new WaitForSeconds(0);
+        interactButtonFillImage.fillAmount = 0;
+        PlayerInputEvent.OnPlayerInteract();
     }
     void Update()
     {
         Move();
+        if (npcs.Count > 0)
+        {
+            IdentifyClosestNPC();
+            CreateInteractButton();
+            SetInteractButtonPosition();
+        }
+        if (npcs.Count == 0)
+            DestroyInteractButton();
     }
 
     void IdentifyClosestNPC()
@@ -92,9 +134,33 @@ public class PlayerController : MonoBehaviour
                     closestNPC = npcs[i];
                 }
             }
-            //closestNPC.GetComponent<SampleNPC>().isClosestToPlayer = true;
             Debug.Log($"{closestNPC.name} is the closest NPC to the player.");
         }
+    }
+
+    void CreateInteractButton()
+    {
+        if (closestNPC == null) return;
+        if(currentInteractButton != null) return;
+        currentInteractButton = Instantiate(interactButtonPrefab, closestNPC.transform.position, Quaternion.identity);
+        interactButtonFillImage = currentInteractButton.transform.GetChild(0).GetChild(0).GetComponentInChildren<Image>();
+        print(interactButtonFillImage.name); 
+    }
+
+    void SetInteractButtonPosition()
+    {
+        if (closestNPC == null) return;
+        if (currentInteractButton != null)
+        {
+            currentInteractButton.transform.SetParent(closestNPC.transform);
+            currentInteractButton.transform.localPosition = new Vector3(0, 2, 0);
+        }
+    }
+
+    void DestroyInteractButton()
+    {
+        if (currentInteractButton != null)
+            Destroy(currentInteractButton);
     }
 
     void OnTriggerEnter(Collider other)
