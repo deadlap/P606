ce75dac9ddd083c819355e2e70da30c5e24466dc
DeadPieGamer <felix@krog.dk>
2025-03-21 16:09:12 +0100

    NPCs now shadows when not in main view
    We need to remove their names as well though

---
+++ Assets/Scripts/Environment/ShadowPerson.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace RoomFocusing
+{
+    public class ShadowPerson : MonoBehaviour
+    {
+        [SerializeField] private GameObject mainLook;
+        [SerializeField] private GameObject shadowLook;
+
+        private int playersCanSee = 0;
+
+        public void AddPlayers(int howMany = 1)
+        {
+            playersCanSee += howMany;
+            UpdateLooks();
+        }
+
+        public void RemovePlayers(int howMany = 1)
+        {
+            playersCanSee -= howMany;
+            UpdateLooks();
+        }
+
+        private void UpdateLooks()
+        {
+            mainLook.SetActive(playersCanSee > 0);
+            shadowLook.SetActive(playersCanSee < 1);
+        }
+    }
+}
