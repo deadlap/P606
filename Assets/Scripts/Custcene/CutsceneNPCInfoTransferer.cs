93e3034f40b071a2abda50a24bedea727b11968f
DeadPieGamer <felix@krog.dk>
2025-04-10 17:29:25 +0200

    Saxophone and stuff in resources

--- Assets/Scripts/Custcene/CutsceneNPCInfoTransferer.cs
+++ Assets/Scripts/Custcene/CutsceneNPCInfoTransferer.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace Cutscene
@@ -23,7 +24,80 @@ namespace Cutscene
         /// <param name="infoToTransfer">The info about this NPC's looks and occupation</param>
         public void TransferInfo(CutsceneNPCInfo infoToTransfer)
         {
+            infoToTransfer = CheckNullValues(infoToTransfer);
+
             IntroCutsceneManager.instance.InformOfNPC(infoToTransfer);
+        }
+
+        /// <summary>
+        /// Transfers the identity of this NPC to the CutsceneManager, so that the NPC can be rendered correctly and do the correct actions in the cutscene.
+        /// </summary>
+        /// <param name="npcIdentity"></param>
+        /// <param name="bodyMat"></param>
+        /// <param name="headMat"></param>
+        public void TransferInfo(Identity npcIdentity, Material bodyMat = null, Material headMat = null, GameObject extraClothing = null)
+        {
+            TransferInfo(new CutsceneNPCInfo(bodyMat, headMat, extraClothing, npcIdentity.Occupation, npcIdentity.name));
+        }
+
+        private CutsceneNPCInfo CheckNullValues(CutsceneNPCInfo info)
+        {
+            if (info.bodyMaterial == null)
+            {
+                Debug.Log($"{name} had no body material assigned, trying to find one automatically");
+                if (!TryGetMaterial("Body", out Material newBodyMat))
+                {
+                    Debug.LogWarning($"No body material found for {name}");
+                }
+                else
+                {
+                    info.bodyMaterial = newBodyMat;
+                }
+            }
+            if (info.headMaterial == null)
+            {
+                Debug.Log($"{name} had no head material assigned, trying to find one automatically");
+                if (!TryGetMaterial("Head", out Material newHeadMat))
+                {
+                    Debug.LogWarning($"No body material found for {name}");
+                }
+                else
+                {
+                    info.headMaterial = newHeadMat;
+                }
+            }
+            if (info.hat == null)
+            {
+                Debug.Log($"{name} had no hat assigned, trying to find one automatically");
+                if (!TryGetMaterial("Head", out Material newHeadMat))
+                {
+                    Debug.Log($"{name} has no hat, very sad ;(");
+                }
+                else
+                {
+                    info.hat = transform.Find("Head").GetChild(0).gameObject;
+                }
+            }
+            if (info.occupation == Identity.Occupations.None) Debug.LogWarning($"{name} has no job?");
+            if (info.myName == null)
+            {
+                Debug.Log($"{name} had no name assigned, automatically assigning it as {name}");
+                info.myName = name;
+            }
+
+            return info;
+        }
+
+        private bool TryGetMaterial(string part, out Material partMat)
+        {
+            partMat = null;
+            if (!transform.Find(part).TryGetComponent(out Renderer partRenderer))
+            {
+                return false;
+            }
+
+            partMat = partRenderer.materials[1];
+            return true;
         }
     }
 }
