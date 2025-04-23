4c2eef3cd6fa6695e22f300c242fccbf2c42c3ac
Akaidus <nikolajjensen6854@gmail.com>
2025-04-23 10:30:54 +0200

    navne clipper ikke i hatte (temp fix)

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class LookAtCamera : MonoBehaviour
 {
     Transform cameraTf;
+    float offset = 0.3f;
     void Awake()
     {
         cameraTf = Camera.main.transform;
         transform.localScale = new Vector3(1, 1, 1);
+        transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z + offset); //quick fix for avoiding hats clipping the name.
     }
 
     void LateUpdate()
