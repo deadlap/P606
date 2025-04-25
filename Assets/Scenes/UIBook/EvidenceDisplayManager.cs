f2e1b7be0cab5bfac6130517979e1c0272449b17
Malthe333 <93973568+Malthe333@users.noreply.github.com>
2025-04-25 12:34:27 +0200

    Bog Færdig Implementation

--- Assets/Scenes/UIBook/EvidenceDisplayManager.cs
+++ Assets/Scenes/UIBook/EvidenceDisplayManager.cs
@@ -1,13 +1,32 @@
 using TMPro;
 using UnityEngine;
+using System;
 
 public class EvidenceDisplayManager : MonoBehaviour
 {
     public static EvidenceDisplayManager Instance;
 
-    [Header("UI Elements")]
 
-    public TextMeshProUGUI evidenceNumberText;
+    public enum EvidenceType{
+        Knife,
+        PocketWatch,
+        Diary,
+        Note,
+    }
+ 
+    public static event Action<EvidenceType> OnPickUpEvidence;
+
+    [Header("Evidence")]
+
+    public GameObject knife;
+    public GameObject pocketWatch;
+    public GameObject diary;
+    public GameObject note;
+
+   
+   [Header("UI Elements")]
+
+    public TextMeshProUGUI evidenceNumberText; 
     public TextMeshProUGUI descriptionText;
     public TextMeshProUGUI ownerText;
     public TextMeshProUGUI mentionedByText;
@@ -17,6 +36,8 @@ public class EvidenceDisplayManager : MonoBehaviour
         Instance = this;
     }
 
+
+    
     public void DisplayEvidence(Evidence evidence)
     {
         
@@ -30,4 +51,28 @@ public class EvidenceDisplayManager : MonoBehaviour
             mentionedByText.text += "- " + name + "\n";
         }
     }
+
+    
+   
+
+    private void ShowEvidence(EvidenceType evidenceType)
+    {
+        switch (evidenceType)
+        {
+            case EvidenceType.Knife:
+                knife.SetActive(true);
+                break;
+            case EvidenceType.PocketWatch:
+                pocketWatch.SetActive(true);
+                break;
+            case EvidenceType.Diary:
+                diary.SetActive(true);
+                break;
+            case EvidenceType.Note:
+                note.SetActive(true);
+                break;
+        }
+    }
+
+
 }
