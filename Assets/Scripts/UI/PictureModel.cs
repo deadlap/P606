88786aa45c6cd1808cb5b7ffe3e4231db76c1cd6
DeadPieGamer <felix@krog.dk>
2025-04-28 20:46:40 +0200

    NPC pictures can be taken

--- Assets/Scripts/UI/PictureModel.cs
+++ Assets/Scripts/UI/PictureModel.cs
@@ -1,16 +1,25 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PictureModel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Renderer bodyRenderer;
+    [SerializeField] private Renderer headRenderer;
+
+    public void AssignLooks(Material headMat, Material bodyMat, GameObject hat, GameObject carriable)
     {
-        
+        Debug.Log($"I'm gonna be assigning my bodyMat as {bodyMat}, and headMat as {headMat}");
+        UpdateMaterial(bodyRenderer, bodyMat);
+        UpdateMaterial(headRenderer, headMat);
+        if (hat != null) Instantiate(hat, headRenderer.transform);
+        if (carriable != null) Instantiate(carriable, bodyRenderer.transform);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void UpdateMaterial(Renderer renderer, Material material)
     {
-        
+        List<Material> oldAndNewMats = new List<Material>(renderer.materials);
+        oldAndNewMats[1] = material;
+        renderer.SetMaterials(oldAndNewMats);
     }
 }
--- Assets/Scripts/UI/TakeNPCPicture.cs
+++ Assets/Scripts/UI/TakeNPCPicture.cs
@@ -1,23 +1,74 @@
+using Cutscene;
+using RoomFocusing;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TakeNPCPicture : MonoBehaviour
 {
+    #region For book to take picture of all NPCs when it is first opened
+    [HideInInspector] protected static List<TakeNPCPicture> npcSelfieCams { get; private set; } = new List<TakeNPCPicture>();
+
+    private void OnEnable()
+    {
+        npcSelfieCams.Add(this);
+    }
+
+    private void OnDisable()
+    {
+        npcSelfieCams.Remove(this);
+    }
+    #endregion
+
     [SerializeField] private Camera m_Camera;
     [SerializeField] private RenderTexture basicTextureSetup;
     public RenderTexture outPutTexture { get; private set; }
     [SerializeField] private RawImage testShowcase;
+    [SerializeField] private GameObject model;
+
+    [Header("NPC look info")]
+    [SerializeField] private Material[] bodyMats;
+    [SerializeField] private GameObject[] hats;
+    [SerializeField] private GameObject[] carriables;
+
+    public static void TakeAllPictures()
+    {
+        foreach(TakeNPCPicture selfieCam in npcSelfieCams)
+        {
+            selfieCam.TakePicture();
+        }
+    }
 
     public void TakePicture()
     {
+        int identityID = (int)GetComponent<Identity>().Occupation;
+        Debug.Log($"{name} got Occupation #{identityID}, which means bodyMat {bodyMats[identityID].name} ({bodyMats[identityID]})");
+        GetComponent<ShadowPerson>().GetLooks();
+        Material headMat = GetComponent<ShadowPerson>().originalHead[1];
+        model.GetComponent<PictureModel>().AssignLooks(headMat, bodyMats[identityID], hats[identityID], carriables[identityID]);
         testShowcase.texture = outPutTexture;
-        m_Camera.gameObject.SetActive(false);
+        // Maybe delay the deactivation with a frame
+        StartCoroutine(DelayDeath());
+    }
+
+    private IEnumerator DelayDeath()
+    {
+        // Wait a frame
+        yield return null;
+
+        // Kill the model
+        model.SetActive(false);
+        Destroy(this);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        model = Instantiate(model);
+        model.transform.position += Vector3.left * 10 * npcSelfieCams.IndexOf(this);
         outPutTexture = new RenderTexture(basicTextureSetup);
+        m_Camera = model.GetComponentInChildren<Camera>();
         m_Camera.targetTexture = outPutTexture;
     }
 }
