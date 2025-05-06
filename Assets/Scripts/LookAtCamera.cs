e8553947d19e89492fa644690fe12b788cdf7e8a
DeadPieGamer <felix@krog.dk>
2025-05-06 22:14:56 +0200

    Name in front of hat

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 public class LookAtCamera : MonoBehaviour
 {
     Transform cameraTf;
-    float offset = 0.3f;
+    //float offset = 0.3f;
     void Awake()
     {
         cameraTf = Camera.main.transform;
         transform.localScale = new Vector3(1, 1, 1);
-        transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z + offset); //quick fix for avoiding hats clipping the name.
+        //transform.position = new Vector3(transform.position.x - offset, transform.position.y, transform.position.z + offset); //quick fix for avoiding hats clipping the name.
     }
 
     void LateUpdate()
